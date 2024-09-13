82ea19664685ec97ebc2d405185c8a7cd3877027
gtauber <gabri.tauber@gmail.com>
2024-09-13 01:12:06 -0300

    feat: Changes TP04 PB

--- src/main/java/college/pb/managementunitapi/ManagementUnitApiApplication.java
+++ src/main/java/college/pb/managementunitapi/ManagementUnitApiApplication.java
@@ -2,8 +2,10 @@ package college.pb.managementunitapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ManagementUnitApiApplication {
 
     public static void main(String[] args) {
